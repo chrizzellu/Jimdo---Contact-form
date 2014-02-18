1417aa68080edb24223b150ea8aa9bbc9bc0602c
jimdoLian <lian@jimdo.com>
2014-02-18 19:44:07 +0100

    button styling

---
+++ js/texts.js
@@ -0,0 +1,29 @@
+var subjects_german = [
+    {
+        "key": "login",
+        "text": "Login und Passwort"
+    },
+    {
+        "key": "presales",
+        "text": "Fragen vor dem Kauf"
+    },
+    {
+        "key": "trial",
+        "text": "Kostenlose Testversion"
+    },
+    {
+        "key": "domain",
+        "text": "Fragen zu Domains"
+    },
+    {
+        "key": "email",
+        "text": "Email Accounts"
+    },
+    {
+        "key": "seo",
+        "text": "Suchmaschinenoptimierung"
+    },
+    {
+        "key": ""
+    }
+]
